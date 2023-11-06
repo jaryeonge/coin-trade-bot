7701be6d834263503f05472dea39bd653ed4a5b3
jaryeonge <jaryeonge@gmail.com>
2023-11-06 09:20:01 +0900

    Innitial commit

---
+++ src/main/java/com/jr/coin/trade/bot/CoinTradeBotApplication.java
@@ -0,0 +1,13 @@
+package com.jr.coin.trade.bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinTradeBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoinTradeBotApplication.class, args);
+    }
+
+}
